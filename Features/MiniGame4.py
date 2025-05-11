605525c5f2545b48cbc307def7c360b5e45e57a8
Jonathan <196909340+jonathan-0612@users.noreply.github.com>
2025-05-11 23:47:02 +0800

    Feat: Added notification icon to alert player for available VM upgrade

--- Features/MiniGame4.py
+++ Features/MiniGame4.py
@@ -1,117 +1,118 @@
 import pygame
-
-# Variable for MG4
-buyingprices = [[300, 400, 500], [800, 900, 1000]]
-sellingprices = [[150, 350, 600], [400, 850, 1350]]
-vm_colour = [[(50, 41, 71), (207, 255, 112), (255, 250, 209), (77, 166, 255)], 
-             [(235, 23, 23), (102, 255, 227), (39, 39, 54)]]
-btncolour = [(15, 97, 43), None]
-menubtnpos = (975, 50)
-btnposX = [130, 630]
-btnposY = [250, 300, 380]
-txtboxposX = [130, 630]
-txtboxposY = [[340, 360], [290, 310], 420]
-txtrectsize = [(50, 20), (80, 20), (90, 20)]
-txtinfo = ["f\"{buyingprices[i][vm_level[i]]}$\"", 
-          "f\"({vm_income[i][vm_level[i]]}$/s)\"",
-          "f\"({sellingprices[i][vm_level[i] - 1]}$)\""]
-
-def greyscale(surf, colours):
-        if len(colours) > 1: surf = greyscale(surf, colours[1:])
-        colour = colours[0] if colours else None
-        image = pygame.Surface(surf.get_size())
-        newRGB = (colour[0] + colour[1] + colour[2]) / 3 if colour else 1
-        image.fill((newRGB, newRGB, newRGB))
-        surf.set_colorkey(colour)
-        image.blit(surf, (0,0))
-        return image
+from Features import Functions
 
 class MG4():
-    def __init__(self, S, W, H, var_dict, E=None):
+    def __init__(self, W, H):
         global temp_vm, mpchange
-        self.var_dict, temp_vm, mpchange = var_dict, var_dict['vm_level'], None
+        temp_vm, mpchange = [], None
+        self.infolist = []
+        self.buyingprices = [[300, 400, 500], [800, 900, 1000]]
+        self.sellingprices = [[150, 350, 600], [400, 850, 1350]]
+        self.menubtnpos = (975, 50)
+        self.btnposX = [130, 630]
+        self.btnposY = [250, 300, 380]
+        self.txtboxposX = [130, 630]
+        self.txtboxposY = [[340, 360], [290, 310], 420]
+        self.txtrectsize = [(50, 20), (80, 20), (90, 20)]
+        self.txtinfo = ["f\"{self.buyingprices[i][vm_level[i]]}$\"", 
+                    "f\"({vm_income[i][vm_level[i]]}$/s)\"",
+                    "f\"({self.sellingprices[i][vm_level[i] - 1]}$)\""]
         
-        if E: self.eventhandler(E)
-        self.update()
-        if not E: self.draw(S, W, H)
+        self.mg4_base = pygame.transform.scale(pygame.image.load("Assets/Images/MG4_Base.png").convert(), (W, H))
+        self.vm1 = pygame.image.load("Assets/Images/MG4_VM1.png").convert_alpha()
+        self.vm1rect = self.vm1.get_rect(center = (350, 375))
+        self.vm2 = pygame.image.load("Assets/Images/MG4_VM2.png").convert_alpha()
+        self.vm2rect = self.vm2.get_rect(center = (860, 375))
+        self.menubtn = pygame.image.load("Assets/Images/MAIN_Menubtn.png").convert_alpha()
+        self.menubtnrect = self.menubtn.get_rect(center = self.menubtnpos)
 
-    def eventhandler(self, E):
+    def eventhandler(self, E, var_dict):
         if E.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = E.pos
-            if self.exitbtnrect.collidepoint(mouse_x, mouse_y): self.var_dict['mg_state'] = None
-            else: self.buttons.update(mouse_x, mouse_y, self.var_dict['VM_EVENT'])
+            if self.menubtnrect.collidepoint(E.pos): 
+                self.var_dict['mg_state'] = None
+            else: self.buttons.update(E, self.var_dict['VM_EVENT'])
         
         elif E.type == pygame.MOUSEMOTION:
             global collidebtn
             collidebtn = False
-            mouse_x, mouse_y = E.pos
+            E.pos = E.pos
 
-            if self.exitbtnrect.collidepoint(mouse_x, mouse_y): 
+            if self.menubtnrect.collidepoint(E.pos): 
                 collidebtn = True
                 pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
                 
-            else: self.buttons.update(mouse_x, mouse_y, self.var_dict['VM_EVENT'], True)
+            else: self.buttons.update(E, self.var_dict['VM_EVENT'], True)
 
             if not collidebtn: pygame.mouse.set_cursor()
 
-    def update(self):
-        self.var_dict['mpc'], self.var_dict['vm_level'] = mpchange, temp_vm
-        pass
+    def update(self, mg_state, var_dict):
+        global temp_vm, mpchange
+        self.var_dict, temp_vm = var_dict, var_dict['vm_level']
+
+        if mg_state == "mainpage":
+            mp, Sfont = self.var_dict['mp'], self.var_dict['Sfont']
+            vm_level, vm_income = self.var_dict['vm_level'], self.var_dict['vm_income']
+            self.buttons, self.infolist = pygame.sprite.Group(), []
+            self.vm1surf, self.vm2surf = self.vm1, self.vm2
+            for i in range(len(vm_level)):  # Determine which btn to display based on lvl
+                if not vm_level[i]: 
+                    btnpaths = ["Buybtn"]
+                    if not i: self.vm1surf = pygame.transform.grayscale(self.vm1) # Greyscale vm that are not bought
+                    else: self.vm2surf = pygame.transform.grayscale(self.vm2) 
+                elif vm_level[i] <= 2: btnpaths = ["Upgradebtn", "Sellbtn"]
+                else: btnpaths = ["Maxbtn", "Sellbtn"]
+
+                for btnpath in btnpaths: self.buttons.add(Buttons(i, btnpath, mp, vm_level[i], self.btnposX, self.btnposY, self.buyingprices, self.sellingprices))
+
+            for i in range(len(vm_level)):
+                if vm_level[i] <= 2:
+                    for j in range(2):
+                        surf = Sfont.render(eval(self.txtinfo[j]), True, 'Black')
+                        k = 1 if vm_level[i] else 0
+                        rect = pygame.rect.Rect((0, 0), self.txtrectsize[j])
+                        rect.center = (self.txtboxposX[i], self.txtboxposY[k][j])
+                        self.infolist.append([surf, rect])
+
+                if vm_level[i]:
+                    surf = Sfont.render(eval(self.txtinfo[2]), True, 'Black')
+                    if len(str(self.sellingprices[i][vm_level[i] - 1])) <= 3:
+                        rect = pygame.rect.Rect((0, 0), self.txtrectsize[1])
+                        rect.center = (self.txtboxposX[i], self.txtboxposY[2])
+                    else:
+                        rect = pygame.rect.Rect((0, 0), self.txtrectsize[2])
+                        rect.center = (self.txtboxposX[i], self.txtboxposY[2])
+                    self.infolist.append([surf, rect])
+
+        self.var_dict['hp'], self.var_dict['mp'] = Functions.update_stats(self.var_dict['hp'], self.var_dict['mp'], mpchange=mpchange)
+        if mpchange: 
+            if mpchange < 0: Functions.add_floating_text(f"{mpchange}", 'mp', (128, 128, 128))
+            else: Functions.add_floating_text(f"+{mpchange}", 'mp', (128, 128, 128))
+        mpchange, self.var_dict['vm_level'] = None, temp_vm
+
+        if mg_state == "mainpage": self.var_dict['displaystats'] = True
+        elif mg_state == "menu": self.var_dict['displaystats'] = False
     
-    def draw(self, S, W, H):
-        mp, Sfont = self.var_dict['mp'], self.var_dict['Sfont']
-        vm_level, vm_income = self.var_dict['vm_level'], self.var_dict['vm_income']
-        
-        colourlist = []
-        mg4_base = pygame.transform.scale(pygame.image.load("Assets/Images/MG4_Base.png").convert(), (W, H))
-        exitbtn = pygame.image.load("Assets/Images/MGE_Exitbtn.png").convert_alpha()
-        self.exitbtnrect = exitbtn.get_rect(center = menubtnpos)
-        
-        self.buttons = pygame.sprite.Group()
-        for i in range(len(vm_level)):     # Determine which btn to display based on lvl
-            if not vm_level[i]: btnpaths = ["Buybtn"]
-            elif vm_level[i] <= 2: btnpaths = ["Upgradebtn", "Sellbtn"]
-            else: btnpaths = ["Maxbtn", "Sellbtn"]
+    def draw(self, S, mg_state):
+        S.blit(self.mg4_base, (0, 0))
+        if mg_state == "mainpage": self.mainpage(S)
+        elif mg_state == "menu": self.menu(S)
 
-            for btnpath in btnpaths: self.buttons.add(Buttons(i, btnpath, mp, vm_level[i]))
-
-            if not vm_level[i]: colourlist.extend(vm_colour[i])    # Greyscale vm that are not bought
-
-        mg4_base = greyscale(mg4_base, colourlist)
-        S.blit(mg4_base, (0, 0))
-        S.blit(exitbtn, self.exitbtnrect)
+    def mainpage(self, S):
+        S.blit(self.menubtn, self.menubtnrect)
+        S.blit(self.vm1surf, self.vm1rect)
+        S.blit(self.vm2surf, self.vm2rect)
+        for surf, rect in self.infolist: S.blit(surf, rect)
         self.buttons.draw(S)
-        self.displayinfo(S, vm_level, vm_income, Sfont)        
+        Functions.draw_floating_texts(S, self.var_dict['Mfont'])
 
-    def displayinfo(self, S, vm_level, vm_income, Sfont):
-        infolist = []
-        # Get info and rect based on vm lvl
-        for i in range(len(vm_level)):
-            if vm_level[i] <= 2:
-                for j in range(2):
-                    surf = Sfont.render(eval(txtinfo[j]), True, 'Black')
-                    k = 1 if vm_level[i] else 0
-                    rect = pygame.rect.Rect((0, 0), txtrectsize[j])
-                    rect.center = (txtboxposX[i], txtboxposY[k][j])
-                    infolist.append([surf, rect])
-
-            if vm_level[i]:
-                surf = Sfont.render(eval(txtinfo[2]), True, 'Black')
-                if len(str(sellingprices[i][vm_level[i] - 1])) <= 3:
-                    rect = pygame.rect.Rect((0, 0), txtrectsize[1])
-                    rect.center = (txtboxposX[i], txtboxposY[2])
-                else:
-                    rect = pygame.rect.Rect((0, 0), txtrectsize[2])
-                    rect.center = (txtboxposX[i], txtboxposY[2])
-                infolist.append([surf, rect])
+    def manu(self, S):
+        pass
         
-        for surf, rect in infolist: S.blit(surf, rect)
-
 class Buttons(pygame.sprite.Sprite):
-    def __init__(self, vm, btnpath, mp, vm_level):
+    def __init__(self, vm, btnpath, mp, vm_level, btnposX, btnposY, buyingprices, sellingprices):
         super().__init__()
         self.id = vm
-        self.image = pygame.image.load(f"Assets/Images/MGE_{btnpath}.png").convert_alpha()
+        self.image = pygame.image.load(f"Assets/Images/MGE_{btnpath}.png").convert_alpha() 
         X_pos = btnposX[self.id]
 
         if btnpath == "Buybtn" or btnpath == "Upgradebtn": 
@@ -120,8 +121,7 @@ class Buttons(pygame.sprite.Sprite):
             self.attribute = 1
             if mp < (-self.mpc):
                 self.clickable = False
-                self.image = greyscale(self.image, btncolour)   # Greyscale btns that cannot afford
-                self.image.set_colorkey((1, 1, 1)) 
+                self.image = pygame.transform.grayscale(self.image)
 
             else: self.clickable = True
 
@@ -131,17 +131,17 @@ class Buttons(pygame.sprite.Sprite):
         elif btnpath == "Sellbtn": 
             self.clickable, self.attribute = True, 0
             Y_pos = btnposY[2]
-            self.mpc = sellingprices[self.id][vm_level - 1]
-
-        self.rect = self.image.get_rect(center = (X_pos, Y_pos))                                         
-
-    def update(self, mouse_x, mouse_y, VM_EVENT, updatecursor=False):
+            self.mpc = (+sellingprices[self.id][vm_level - 1])
+        
+        self.rect = self.image.get_rect(center = (X_pos, Y_pos))      
+                                                                 
+    def update(self, E, VM_EVENT, updatecursor=False):
         if updatecursor: 
-            self.updatecursor(mouse_x, mouse_y)
+            self.updatecursor(E)
             return
         
         global temp_vm, mpchange
-        if self.rect.collidepoint(mouse_x, mouse_y): 
+        if self.rect.collidepoint(E.pos): 
             if self.clickable:
                 mpchange = self.mpc
                 if self.attribute:
@@ -150,10 +150,10 @@ class Buttons(pygame.sprite.Sprite):
                 else: 
                     temp_vm[self.id] = 0
                     pygame.time.set_timer(VM_EVENT[self.id], 0)
-     
-    def updatecursor(self, mouse_x, mouse_y):
+
+    def updatecursor(self, E):
         global collidebtn
-        if self.rect.collidepoint(mouse_x, mouse_y):
+        if self.rect.collidepoint(E.pos):
             collidebtn = True
             if self.clickable: pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
             else: pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_NO)
